55ddbcfe33c002e6af5c05e5cdfb7b6d9d49ef73
Nathan Carpenter <ncarpenter@lshigh.org>
2023-03-09 12:06:17 -0800

    Add files via upload

---
+++ Unit-2/Notes/For loops notes.py
@@ -0,0 +1,26 @@
+#for loop 1
+
+#WHILE loops repead until some condition is met (you dont know how many times it will do)
+
+#For loops repeat an EXACT number of times (You do know how many times it will do)
+
+#UNLIKE other languages, FOR loops in python are desinged to lloop over a COLLECTION OF ITEMS (lists, dictionaries, range(), objects)
+#####################
+#THREE major uses
+
+#1 analyzing LETTERS in a STRING
+word = 'horsefeathers'
+
+##for each in word:
+	#each will be 'h'
+	#print('the letter is',each)
+	#print('the letter',each,'shows up',word.count(each),'lines')
+	
+	
+
+#2 analyzing OBJECTS in a LIST (or Dictionary)
+
+#anylist = ['horse','donkey','zebra','alpaca','elephant','unicorn','giraffe']
+
+#for chickenfeathers in mylist:
+	# print(chickenfeathers,'is in the',mylist.index(chickenfeathers))
